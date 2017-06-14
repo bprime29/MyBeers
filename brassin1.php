810167281920cbdfac37919cd3f0119d727f36d0
Prime Bertrand <bprime.ext@orange.com>
2017-06-14 17:25:38 +0200

    debut brassin1

---
+++ brassin1.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bprime
+ * Date: 6/14/17
+ * Time: 4:31 PM
+ */
+$titre = "Brassin 1";
+include("includes/config.php");
+include("includes/debut.php");
+?>
+<div class="container">
+
+    <div class="starter-template">
+        <h1>Blonde simple au combawa</h1>
+        <p class="lead">Pour mon premier brassin, je suis parti sur une recette de bière blonde simple. J'y ai rajouté un combawa pour lui rajouter une petite note exotique.</p>
+    </div>
+    <div align="center">
+        <table class="table1">
+            <tbody>
+            <tr>
+                <td class="topleft">
+                    Type: Monopalier
+                </td>
+                <td class="topright">
+                    Date: xx:22x2017
+                </td>
+            </tr>
+            <tr>
+                <td class="topleft">
+                    Batch size: 20l
+                </td>
+                <td class="topright">
+                    Brasseur: Bertrand
+                </td>
+            </tr>
+            <tr>
+                <td class="topleft">
+                    Boil size: 20l
+                </td>
+                <td class="topright">
+                    Boil time: 80min
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" bgcolor="#a9a9a9">
+                    <p class="text-center">
+                        Ingrédients
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <table class="ingredient"
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div><!-- /.container -->
