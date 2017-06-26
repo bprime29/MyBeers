f193ab6d312c5fb2a6476328e740988a3f0a15db
bprime <bertrand.prime@gmail.com>
2017-06-26 23:33:22 +0200

    brassin2

--- brassin1.php
+++ brassin1.php
@@ -14,6 +14,7 @@ include("includes/debut.php");
     <div class="col-lg-10 col-lg-offset-1">
         <h1>Blonde simple au combava</h1>
         <p class="lead">Pour mon premier brassin, je suis parti sur une recette de bière blonde simple. J'y ai rajouté un combava pour lui rajouter une petite note exotique.</p>
+        <p align="center">Quelques photos du brassin <a href="https://goo.gl/photos/6BA9mZiJzFsnFB9v8">ici</a></p>
     </div>
     <div align="center">
         <table class="table1">
--- brassin2.php
+++ brassin2.php
@@ -13,7 +13,8 @@ include("includes/debut.php");
 
     <div class="starter-template">
         <h1>English India Pale Ale</h1>
-        <p class="lead">Pour mon premier brassin, je suis parti sur une recette de bière blonde simple. J'y ai rajouté un combawa pour lui rajouter une petite note exotique.</p>
+        <p class="lead">Pour mon deuxieme brassin, j'ai voulu tenter une recette d'India Pale Ale classique. Je me suis beaucoup inspiré d'une recette du site du nain brasseur. (<a href="http://univers-biere.net">http://univers-biere.net</a>)</p>
+        <p align="center">Quelques photos du brassin <a href="https://goo.gl/photos/bxsVtosVEL5FhTADA">ici</a></p>
     </div>
     <div align="center">
         <table class="table1">
@@ -54,6 +55,129 @@ include("includes/debut.php");
                     <table class="ingredient"
                 </td>
             </tr>
+            <tr>
+                <td colspan="2">
+                    <table class="ingredient">
+                        <tbody>
+                        <tr>
+                            <th width="16%" align="left">Amount</th>
+                            <th width="51%" align="left">Item</th>
+                            <th width="15%" align="left">Type</th>
+                            <th width="16%" align="left">% or IBU</th
+                        </tr>
+                        <tr>
+                            <td align="left">6,00 kg</td>
+                            <td align="left">Pale Malt (2 Row) Bel</td>
+                            <td align="left">Grain</td>
+                            <td align="left">96,77 %</td>
+                        </tr>
+                        <tr>
+                            <td align="left">0.20 kg</td>
+                            <td align="left">Caramel/Crystal Malt - 60L (150,0 EBC)</td>
+                            <td align="left">Grain</td>
+                            <td align="left">3.23 %</td>
+                        </tr>
+                        <tr>
+                            <td align="left">60,00 g</td>
+                            <td align="left">Goldings, East Kent [5,40 %] (80 min)</td>
+                            <td align="left">Hops</td>
+                            <td align="left">32,5 IBU</td>
+                        </tr>
+                        <tr>
+                            <td align="left">10,00 g</td>
+                            <td align="left">Northern Brewer [8,40 %] (80 min)</td>
+                            <td align="left">Hops</td>
+                            <td align="left">8,4 IBU</td>
+                        </tr>
+                        <tr>
+                            <td align="left">10,00 g</td>
+                            <td align="left">Goldings, East Kent [5,40 %] (10 min)</td>
+                            <td align="left">Hops</td>
+                            <td align="left">1,9 IBU</td>
+                        </tr>
+                        <tr>
+                            <td align="left">1 Pkgs</td>
+                            <td align="left">SafAle English Ale (DCL Yeast #S-04)</td>
+                            <td align="left">Yeast-Ale</td>
+                            <td align="left"></td
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="2" bgcolor="#a9a9a9">
+                    <p class="text-center">
+                        Profile de la bière
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td width="40%">
+                    <p style="margin-top: 0; margin-bottom: 0"><b><i>Est Original
+                                Gravity</i>:</b>
+                        1,071 SG</td>
+                <td width="52%"><i><b>Est Final Gravity:</b></i> 1,018 SG</td>
+            </tr>
+            <tr>
+                <td width="40%">
+                    <b><i>Estimated Alcohol by Vol</i></b><i><b>:</b></i> 7 % </td>
+                <td width="52%"><i><b>Bitterness:</b></i> 42.8 IBU</td>
+            </tr>
+            <tr>
+                <td width="40%">
+                    <i><b>Est Color:</b></i> 17,8 EBC</td>
+                <td width="52%"><b><i>Color</i>:</b> <TABLE><TD BGCOLOR="#be8b3a"><FONT COLOR="#be8b3a"> Color </FONT></TD></TABLE></td>
+            </tr>
+            <tr>
+                <td colspan="2" bgcolor="#a9a9a9">
+                    <p class="text-center">
+                        Mash profile
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td width="40%">
+                    <b><i>Mash Name</i>:</b>
+                    Infusion, Batch Sparge</td>
+                <td width="52%"><b><i>Total Grain Weight</i>:</b>
+                    6,20 kg</td>
+            </tr>
+            <tr>
+                <td width="40%">
+                    <b><i>Sparge Water</i>:</b>
+                    12,8 L</td>
+                <td width="52%"><b><i>Sparge Temperature</i>:</b> 78 C</td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <p class="text-center">
+                    <table width="100%%" cellspacing="0" cellpadding="0">
+                        <caption><u><b>Single Infusion, Full Body, Batch Sparge</b></u></caption>
+
+                        <tbody>
+                        <tr>
+                            <th width="16%" align="left">Step Time</th>
+                            <th width="23%" align="left">Name</th>
+                            <th width="44%" align="left">Description</th>
+                            <th width="16%" align="left">Step Temp</th>
+                        </tr>
+                        <tr>
+                            <td align="left">90 min</td>
+                            <td align="left">Monopalier</td>
+                            <td align="left">Add 12,8 L of water at 72 C</td>
+                            <td align="left">68,0 C</td>
+                        </tr>
+
+                        <tr>
+                            <td align="left">10 min</td>
+                            <td align="left">Mash Out</td>
+                            <td align="left">Heat at 78,0 C</td>
+                            <td align="left">78,0 C</td>
+                        </tr>
             </tbody>
         </table>
     </div>
