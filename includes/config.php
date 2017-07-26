38aa23a41591dd82882068d6bf325f6a370abe28
bprime <bertrand.prime@gmail.com>
2017-07-26 22:47:42 +0200

    config

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,5 @@
 <?php
-
 define('ROOTPATH', 'http://'.$_SERVER['HTTP_HOST'].'/MyBeers', true);
 define('TITRESITE', 'MyBeers', true);
 define('INCLUDESDIR',dirname(__FILE__) . '/');
 $queries = 0;
-
-?>
