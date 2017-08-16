2a7ccaa231cb4f5b4f5da20910e1f06c278d4a4d
bprime <bertrand.prime@gmail.com>
2017-08-16 22:48:59 +0200

    fin.php

---
+++ includes/fin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bebzh
+ * Date: 16/08/2017
+ * Time: 22:46
+ */
+?>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 footerleft ">
+                <div class="logofooter"> Logo</div>
+                <p><?php include ('includes/compteur.php'); ?></p>
+
+</div>
+<div class="col-md-2 col-sm-6 paddingtop-bottom">
+    <h6 class="heading7">GENERAL LINKS</h6>
+    <!--ul class="footer-ul">
+        <li><a href="#"> Career</a></li>
+        <li><a href="#"> Privacy Policy</a></li>
+        <li><a href="#"> Terms & Conditions</a></li>
+        <li><a href="#"> Client Gateway</a></li>
+        <li><a href="#"> Ranking</a></li>
+        <li><a href="#"> Case Studies</a></li>
+        <li><a href="#"> Frequently Ask Questions</a></li>
+    </ul-->
+</div>
+<div class="col-md-3 col-sm-6 paddingtop-bottom">
+    <h6 class="heading7">LATEST POST</h6>
+    <!--div class="post">
+        <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+        <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+        <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+    </div-->
+</div>
+<div class="col-md-3 col-sm-6 paddingtop-bottom">
+    <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-height="300" data-small-header="false" style="margin-bottom:15px;" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+        <div class="fb-xfbml-parse-ignore">
+            <blockquote cite="https://www.facebook.com/facebook"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+</footer>
