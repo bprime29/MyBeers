150b84b556861464c7096b7c482a2f77dd898e82
bprime <bertrand.prime@gmail.com>
2017-08-28 23:05:49 +0200

    microdonnees

--- includes/microdonnees.php
+++ includes/microdonnees.php
@@ -9,36 +9,36 @@
 
 <script type="application/ld+json">
 {
-  "@context": "http://schema.org",
-  "@type": "BreadcrumbList",
-  "itemListElement": [{
-    "@type": "ListItem",
-    "position": 1,
-    "item": {
-      "@id": "https://tybrasseur.fr/",
-      "name": "Accueil"
-    }
-  },{
-    "@type": "ListItem",
-    "position": 2,
-    "item": {
-      "@id": "https://tybrasseur.fr/recettes.php",
-      "name": "Mes recettes"
-    }
-  },{
-    "@type": "ListItem",
-    "position": 3,
-    "item": {
-      "@id": "https://tybrasseur.fr/brassin1.php",
-      "name": "Uploading an image via a REST API with Symfony 3 and Javascript"
-    }
-  },{
-    "@type": "ListItem",
-    "position": 3,
-    "item": {
-      "@id": "https://tybrasseur.fr/brassin2.php",
-      "name": "Uploading an image via a REST API with Symfony 3 and Javascript"
-    }
-  }]
+	"@context": "http://schema.org",
+	"@type": "BreadcrumbList",
+	"itemListElement": [{
+		"@type": "ListItem",
+		"position": 1,
+		"item": {
+			"@id": "https://tybrasseur.fr/",
+			"name": "Accueil"
+		}
+	}, {
+		"@type": "ListItem",
+		"position": 2,
+		"item": {
+			"@id": "https://tybrasseur.fr/recettes.php",
+			"name": "Mes recettes"
+		}
+	}, {
+		"@type": "ListItem",
+		"position": 3,
+		"item": {
+			"@id": "https://tybrasseur.fr/brassin1.php",
+			"name": "Uploading an image via a REST API with Symfony 3 and Javascript"
+		}
+	}, {
+		"@type": "ListItem",
+		"position": 3,
+		"item": {
+			"@id": "https://tybrasseur.fr/brassin2.php",
+			"name": "Uploading an image via a REST API with Symfony 3 and Javascript"
+		}
+	}]
 }
 </script>
