ce51c36b1e1ef00042239a53914f21e42f4678f2
bprime <bertrand.prime@gmail.com>
2017-09-18 20:46:22 +0200

    test calendar

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-define('ROOTPATH', 'https://'.$_SERVER['HTTP_HOST'].'/MyBeers', true);
+define('ROOTPATH', 'https://'.$_SERVER['HTTP_HOST'], true);
 define('TITRESITE', 'Ty Brasseur', true);
 define('INCLUDESDIR',dirname(__FILE__) . '/');
 $queries = 0;
