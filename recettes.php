dc0991b953f11baf440a7f0ed5cecc5f3c17df6e
bprime <bertrand.prime@gmail.com>
2017-10-09 22:28:44 +0200

    modif recettes

--- recettes.php
+++ recettes.php
@@ -18,15 +18,15 @@ include_once("includes/analyticstracking.php");
 
 <div class="container">
 
-    <!--div class="starter-template">
+    <div class="starter-template">
         <h1>Mes recettes de bières</h1>
         <p class="lead">Voici ci dessous les différentes recettes que j'ai testée.</p>
         <li><a href="brassin1.php">Brassin 1 : Blonde simple au combava</a></li>
         <li><a href="brassin2.php">Brassin 2 : English India Pale Ale</a></li>
         <li><a href="brassin3.php">Brassin 3 : Bière douce rubis profond</a></li>
 
-    </div-->
-    <div class="row">
+    </div>
+    <!--div class="row">
         <div class="col-lg-12">
             <h1 class="text-center">Mes recettes de bières</h1>
             <p class="text-center">
@@ -85,6 +85,6 @@ include_once("includes/analyticstracking.php");
                 </li>
             </ul>
         </div>
-    </div>
+    </div-->
 
 </div><!-- /.container -->
